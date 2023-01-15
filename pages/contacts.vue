4c566b69fb882a1a64d3783e4d198bc24bf9a571
MogliCodes <moglicodes@gmail.com>
2023-01-15 15:47:30 +0100

    feat(Contacts): add basic data fetching

--- pages/contacts.vue
+++ pages/contacts.vue
@@ -1,9 +1,29 @@
 <template>
   <div class="container mx-auto">
     <h1 class="text-4xl">Contacts</h1>
+
+    <div class="grid">
+      <div class="card border rounded p-4" v-for="contact in contacts" :key="contact.id">
+        {{ contact.id }}
+        {{ contact.firstname }}
+        {{ contact.lastname }}
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script setup>
-console.log('test')
+const client = useSupabaseClient()
+const user = useSupabaseUser()
+
+async function fetchContacts() {
+  const { data: contacts } = await useAsyncData('contacts', async () => {
+    const { data } = await client.from('Contacts').select('id, userId, firstname, lastname, company').eq('userId', user?.value?.id)
+    return data
+  })
+  return contacts
+}
+const contacts = await fetchContacts()
+
 </script>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="container mx-auto">
     <h1 class="text-4xl">Dashboard</h1>
-    <button @click="client.auth.signOut()">Logout</button>
-    <pre>{{ userSettings }}</pre>
-    <pre>
-      {{ user }}
-    </pre>
   </div>
 </template>
 
