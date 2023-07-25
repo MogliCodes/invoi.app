f8ce2d512ab243908f9ae5ccfd367b864fb7660e
MogliCodes <moglicodes@gmail.com>
2023-07-25 18:47:10 +0200

    chore(deps): update Nuxt to v3.6.5

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -3,37 +3,3 @@
     <h1 class="text-4xl">Dashboard</h1>
   </div>
 </template>
-
-<script setup>
-const client = useSupabaseClient()
-const user = useSupabaseUser()
-
-definePageMeta({
-  middleware: 'auth'
-})
-
-const userSettings = await fetchUserSettings()
-
-async function fetchUserSettings() {
-    const { data: userData } = await useAsyncData('restaurant', async () => {
-      const { data } = await client.from('UserSettings').select('userId').eq('userId', user?.value?.id)
-      return data
-    })
-    if(userData.value.length <= 0 ) {
-      insertInitialUserSettings(user?.value?.id)
-    } else {
-      console.log(`user exists and has id ${userData.value?.[0]?.userId}`)
-    }
-    return userData.value[0].userId
-}
-
-async function insertInitialUserSettings(userId) {
-  const { error } = await client
-      .from('UserSettings')
-      .insert({ userId: userId })
-}
-
-
-
-
-</script>
