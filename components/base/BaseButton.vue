d495f7411108878d9f2b5275a12a3b1d311ee882
MogliCodes <moglicodes@gmail.com>
2023-07-25 19:52:08 +0200

    feat: add base components for button and input

---
+++ components/base/BaseButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="rounded bg-blue-700 px-6 py-3 text-white">
+    {{ props.text }}
+  </button>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  text: string;
+};
+
+const props = withDefaults(defineProps<Props>(), {});
+</script>
--- server/api/register.post.ts
+++ server/api/register.post.ts
@@ -1,5 +1,12 @@
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
-
-  console.log(body);
+  console.log("TEST");
+  const res: Promise<object> = await $fetch(
+    "http://localhost:8000/api/auth/register",
+    {
+      method: "POST",
+      body,
+    }
+  );
+  return res;
 });
