3e5400681debc38902edeacd3fcf189484735d2d
MogliCodes <moglicodes@gmail.com>
2023-07-25 19:52:08 +0200

    feat: add base components for button and input

---
+++ components/base/BaseInput.vue
@@ -0,0 +1,16 @@
+<template>
+  <input class="rounded border-2 p-2" type="text" @input="updateValue" />
+</template>
+
+<script setup lang="ts">
+type Props = {
+  modelValue: String;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const props = defineProps<Props>();
+const emit = defineEmits(["update:modelValue"]);
+const updateValue = (event) => {
+  emit("update:modelValue", event.target.value);
+};
+</script>
--- pages/login.vue
+++ pages/login.vue
@@ -1,28 +1,44 @@
 <template>
   <div class="container mx-auto">
-    <h1 class="text-4xl">Login</h1>
-    <div class="mb-6">
-      <label class="block" for="username">Username</label>
-      <input
-        v-model="username"
-        class="block bg-slate-100 p-4 shadow-lg"
-        type="text"
-      />
+    <div class="flex gap-8">
+      <div>
+        <h1 class="mb-6 text-4xl">Login</h1>
+        <div class="mb-3">
+          <label class="block" for="username">Username</label>
+          <BaseInput v-model="username" />
+        </div>
+        <div class="mb-3">
+          <label class="block" for="password">Password</label>
+          <BaseInput v-model="password" />
+        </div>
+        <BaseButton text="Login" @click="login" />
+      </div>
+      <div>
+        <h1 class="mb-6 text-4xl">Register</h1>
+        <div class="mb-3">
+          <label class="block" for="username">Username</label>
+          <BaseInput v-model="username" />
+        </div>
+        <div class="mb-3">
+          <label class="block" for="username">E-Mail</label>
+          <BaseInput v-model="email" />
+        </div>
+        <div class="mb-3">
+          <label class="block" for="password">Password</label>
+          <BaseInput v-model="password" />
+        </div>
+        <BaseButton text="Register" @click="register" />
+      </div>
     </div>
-    <div>
-      <label class="block" for="password">Password</label>
-      <input
-        v-model="password"
-        class="block bg-slate-100 p-4 shadow-lg"
-        type="password"
-      />
-    </div>
-    <button @click="login">Login</button>
   </div>
 </template>
 
 <script setup lang="ts">
+import BaseInput from "~/components/base/BaseInput.vue";
+import BaseButton from "~/components/base/BaseButton.vue";
+
 const username = ref(null);
+const email = ref(null);
 const password = ref(null);
 
 async function login() {
@@ -31,12 +47,29 @@ async function login() {
       method: "POST",
       body: {
         username,
+        email,
         password,
       },
     });
     console.log("data", data);
   } catch (error) {
     console.log("error", error);
+  }
+}
+
+async function register() {
+  try {
+    const { data } = await useFetch("/api/register", {
+      method: "POST",
+      body: {
+        username,
+        email,
+        password,
+      },
+    });
+    console.log("data", data);
+  } catch (error) {
+    console.error(error);
   }
 }
 </script>
