335d1290f42c16974e8c68f3221d8b97d219a2ea
MogliCodes <moglicodes@gmail.com>
2023-10-22 16:31:34 +0200

    feat: slight design overhaul

--- pages/contacts/index.vue
+++ pages/contacts/index.vue
@@ -1,57 +1,74 @@
 <template>
-  <div class="container mx-auto flex flex-col items-start gap-8">
-    <BaseHeadline class="mb-8" type="h1" text="Contacts" />
-    <BaseButton to="/contacts/create" text="Create contact" variant="yellow" />
+  <div class="container mx-auto flex flex-col items-start">
+    <div class="mb-12">
+      <BaseHeadline class="mb-4" type="h1" text="Contacts" />
+      <BaseButton
+        to="/contacts/create"
+        text="Create contact"
+        variant="yellow"
+      />
+    </div>
     <div class="w-full">
       <div v-if="!contacts.length" class="flex flex-col items-start gap-4">
         <p>No contacts created yet! Start creating your first contact.</p>
       </div>
-      <div
-        class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
-      >
-        <div
-          v-for="contact in contacts"
-          :key="contact.id"
-          class="card rounded border-2 p-4"
-        >
-          {{ contact.id }}<br />
-          {{ contact.firstname }}<br />
-          {{ contact.lastname }}<br />
+      <div>
+        <div class="mb-2">
+          <span class="text-secondary-100 text-sm font-bold"
+            >{{ contacts.length }} Contacts</span
+          >
         </div>
+        <table class="w-full">
+          <thead
+            class="border-x-2 border-t-2 border-yellow-dark bg-blue-80 text-white"
+          >
+            <th class="text-left px-6 py-3">Firstname</th>
+            <th class="text-left px-6 py-3">Lastname</th>
+            <th class="text-left px-6 py-3">DOB</th>
+            <th class="text-left px-6 py-3">Street</th>
+            <th class="text-left px-6 py-3">Zip</th>
+            <th class="text-left px-6 py-3">City</th>
+          </thead>
+          <tbody>
+            <tr
+              v-for="contact in contacts"
+              :key="contact.id"
+              class="card rounded border-2 p-4 even:bg-[rgba(0,0,0,0.05)]"
+            >
+              <td class="px-6 py-3">{{ contact.firstname }}</td>
+              <td class="px-6 py-3">{{ contact.lastname }}</td>
+              <td class="px-6 py-3">
+                {{
+                  new Date(contact.dob)
+                    .toLocaleDateString("en-GB", {
+                      day: "2-digit",
+                      month: "2-digit",
+                      year: "numeric",
+                    })
+                    .split("/")
+                    .join(".")
+                }}
+              </td>
+              <td class="px-6 py-3">{{ contact.street }}</td>
+              <td class="px-6 py-3">{{ contact.zip }}</td>
+              <td class="px-6 py-3">{{ contact.city }}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-const contacts = [
-  {
-    id: "f34u34n3kj43k",
-    firstname: "Otto",
-    lastname: "Nachname",
-    dob: "01.01.1970",
-    street: "Musterstraße",
+import { useAuthStore } from "~/stores/auth.store";
+
+const authStore = useAuthStore();
+
+const { data: contacts } = useFetch(`http://localhost:8000/api/contact`, {
+  headers: {
+    Authorization: `Bearer ${authStore.accessToken}`,
+    ClientId: authStore.userId,
   },
-  {
-    id: "f34u34n3kj43k",
-    firstname: "Otto",
-    lastname: "Nachname",
-    dob: "01.01.1970",
-    street: "Musterstraße",
-  },
-  {
-    id: "f34u34n3kj43k",
-    firstname: "Otto",
-    lastname: "Nachname",
-    dob: "01.01.1970",
-    street: "Musterstraße",
-  },
-  {
-    id: "f34u34n3kj43k",
-    firstname: "Otto",
-    lastname: "Nachname",
-    dob: "01.01.1970",
-    street: "Musterstraße",
-  },
-];
+});
 </script>
