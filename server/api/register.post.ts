d114e106e273b23ded0a0894282cdb1b3547f0b0
MogliCodes <moglicodes@gmail.com>
2023-10-22 16:32:47 +0200

    feat(Contact): add basic controller

--- server/api/register.post.ts
+++ server/api/register.post.ts
@@ -1,12 +1,7 @@
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
-  console.log("TEST");
-  const res: Promise<object> = await $fetch(
-    "http://localhost:8000/api/auth/register",
-    {
-      method: "POST",
-      body,
-    }
-  );
-  return res;
+  return await $fetch("http://localhost:8000/api/auth/register", {
+    method: "POST",
+    body,
+  });
 });
