6708a54abb018d726dc0942d7e8951a36e2a0957
MogliCodes <moglicodes@gmail.com>
2023-10-28 17:12:42 +0200

    feat(Contact): extend controller and views

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devtools: true,
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/supabase", "@pinia/nuxt"],
   supabase: {
     url: process.env.SUPABASE_URL,
