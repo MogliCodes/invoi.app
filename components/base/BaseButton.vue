63be54a0ff145c446ab9ba2530211759dea05555
MogliCodes <moglicodes@gmail.com>
2023-11-25 13:57:28 +0100

    feat(Button): add multiple button sizes

--- components/base/BaseButton.vue
+++ components/base/BaseButton.vue
@@ -2,7 +2,7 @@
   <component
     :is="componentType"
     :to="to"
-    class="inline-block rounded-full px-6 py-3 font-medium"
+    class="inline-block rounded-full font-medium"
     :class="[
       buttonClasses,
       disabled
@@ -22,7 +22,8 @@ type Props = {
   text: string;
   to?: string;
   type?: "button" | "submit";
-  variant?: "yellow" | "secondary" | "outline" | "red";
+  variant?: "yellow" | "secondary" | "outline" | "red" | "amber";
+  size?: "sm" | "md" | "lg";
 };
 
 const NuxtLink = resolveComponent("NuxtLink");
@@ -36,16 +37,23 @@ const props = withDefaults(defineProps<Props>(), {
   type: "button",
   to: "",
   variant: "yellow",
+  size: "md",
 });
 
 const STYLE_MAPS = {
   red: "bg-red-500 text-red-100",
   yellow: "border-2 border-yellow-normal bg-yellow-normal text-yellow-dark",
-  yellowDark: "bg-yellow-400 text-yellow-950",
+  amber: "bg-yellow-400 text-yellow-900",
   outline:
     "border-2 bg-transparent border-blue-900 text-blue-900 dark:bg-transparent dark:border-white dark:text-white",
-  secondary: "bg-secondary-100 text-white",
+  secondary: "border-2 border-secondary-100 bg-secondary-100 text-white",
 };
 
-const buttonClasses = STYLE_MAPS[props.variant];
+const SIZE_MAP = {
+  sm: "px-4 py-2",
+  md: "px-6 py-3",
+  lg: "px-9 py-6",
+};
+
+const buttonClasses = STYLE_MAPS[props.variant] + " " + SIZE_MAP[props.size];
 </script>
