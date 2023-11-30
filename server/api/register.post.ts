7a90591e08940ab01eb85141a894ce25bbaedfcd
MogliCodes <moglicodes@gmail.com>
2023-11-30 17:17:09 +0100

    fix: import base url in auth handlers

--- server/api/register.post.ts
+++ server/api/register.post.ts
@@ -1,6 +1,8 @@
 export default defineEventHandler(async (event) => {
+  const config = useRuntimeConfig();
+  const backendBaseUrl = config.public.backendBaseUrl;
   const body = await readBody(event);
-  return await $fetch("${backendBaseUrl}/api/auth/register", {
+  return await $fetch(`${backendBaseUrl}/api/auth/register`, {
     method: "POST",
     body,
   });
