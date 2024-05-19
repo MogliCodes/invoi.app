5e979e31834b9ce629223383d4c160c00fbd9851
MogliCodes <moglicodes@gmail.com>
2024-05-19 16:06:21 +0200

    chore: remove Scalar from config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
     },
   },
   devtools: true,
-  modules: ["@nuxt/ui", "@nuxtjs/supabase", "@pinia/nuxt", "@scalar/nuxt"],
+  modules: ["@nuxt/ui", "@nuxtjs/supabase", "@pinia/nuxt"],
   supabase: {
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_KEY,
