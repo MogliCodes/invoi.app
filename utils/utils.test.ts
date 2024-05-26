6f5675b852aac28efab4f08ade5c525a16a834d2
MogliCodes <moglicodes@gmail.com>
2024-05-26 14:17:08 +0200

    test: add test for formatting util

--- utils/utils.test.ts
+++ utils/utils.test.ts
@@ -1,5 +1,13 @@
-import { describe, test } from "vitest";
+import { describe, expect, test } from "vitest";
+import { formatCurrencyAmount } from "./utils";
 
 describe("Utils", () => {
-  test.todo("format curreny");
+  test("format amount of money to currency amount string", () => {
+    expect(formatCurrencyAmount(0)).toBe("0,00 €");
+    expect(formatCurrencyAmount(1)).toBe("1,00 €");
+    expect(formatCurrencyAmount(999)).toBe("999,00 €");
+    expect(formatCurrencyAmount(1000)).toBe("1.000,00 €");
+    expect(formatCurrencyAmount(1000000)).toBe("1.000.000,00 €");
+    expect(formatCurrencyAmount(50.5)).toBe("50,50 €");
+  });
 });
--- utils/utils.ts
+++ utils/utils.ts
@@ -1,21 +1,38 @@
-const currencySymbol = {
+type Currency = "EUR" | "USD" | "GBP"; // Add more currency codes as needed
+
+const currencySymbol: Record<Currency, string> = {
   EUR: "€",
+  USD: "$",
+  GBP: "£",
+  // Add other currencies and their symbols here
 };
 export const formatCurrencyAmount = (
   amount: number,
-  currency: string = "EUR",
+  currency: Currency = "EUR",
   locale: string = "de-DE"
 ) => {
-  return (
-    amount
-      .toLocaleString(locale, {
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
-      })
-      .replace(/\.(?=.*\.)/, ",") +
-    " " +
-    currencySymbol[currency]
+  if (!currencySymbol[currency]) {
+    throw new Error(`Currency symbol for ${currency} not found`);
+  }
+
+  const formattedAmount = amount.toLocaleString(locale, {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+  // Check if Intl is available and use it to get the correct decimal separator
+  const decimalSeparator =
+    typeof Intl !== "undefined"
+      ? new Intl.NumberFormat(locale)
+          .formatToParts(1.1)
+          .find((part) => part.type === "decimal")?.value || "."
+      : ".";
+
+  const formattedAmountWithComma = formattedAmount.replace(
+    decimalSeparator,
+    ","
   );
+
+  return `${formattedAmountWithComma} ${currencySymbol[currency]}`;
 };
 
 export const formatAmountToCent = (amount: number): number => {
