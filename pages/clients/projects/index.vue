5e3e2837545b285dbb284dccd32c2b9233423331
MogliCodes <moglicodes@gmail.com>
2024-06-22 13:15:13 +0200

    feat(Projects): add delete functionality

--- pages/clients/projects/index.vue
+++ pages/clients/projects/index.vue
@@ -9,12 +9,15 @@
     </div>
   </section>
   <section v-if="projects" class="mt-10">
-    <table class="overflow-hidden rounded-lg dark:text-gray-400">
+    <table class="w-full overflow-hidden rounded-lg dark:text-gray-400">
       <thead class="bg-blue-90 text-white">
         <tr>
           <th class="px-6 py-5 text-left">Title</th>
           <th class="px-6 py-5 text-left">Client</th>
           <th class="px-6 py-5 text-left">Description</th>
+          <th class="px-6 py-5 text-left">Fortschritt</th>
+          <th class="px-6 py-5 text-left">Budget</th>
+          <th class="px-6 py-5 text-left">Aufgewendete Zeit</th>
           <th class="px-6 py-5 text-right">Actions</th>
         </tr>
       </thead>
@@ -27,6 +30,15 @@
           <td class="px-6 py-3">{{ getClientName(project.client) }}</td>
           <td class="px-6 py-3">{{ project.description }}</td>
           <td class="px-6 py-3">
+            <span class="block relative border-2 w-28 h-2 rounded-full">
+              <span
+                class="block absolute -top-[2px] -left-[2px] left-0 rounded-full w-1/2 h-2 bg-green-300"
+              ></span>
+            </span>
+          </td>
+          <td class="px-6 py-3"></td>
+          <td class="px-6 py-3"></td>
+          <td class="px-6 py-3">
             <span class="flex justify-end gap-2">
               <NuxtLink :to="`/clients/projects`">
                 <UIcon
@@ -37,6 +49,7 @@
               <UIcon
                 class="cursor-pointer text-xl transition-colors hover:text-gray-400 dark:hover:text-white"
                 name="i-heroicons-trash"
+                @click="deleteProject(project._id)"
               />
             </span>
           </td>
@@ -57,7 +70,7 @@ const authStore = useAuthStore();
 const config = useRuntimeConfig();
 const backendBaseUrl = config.public.BACKEND_BASE_URL;
 
-const { data: projects } = useFetch("/api/clients/projects/get", {
+const { data: projects, refresh } = useFetch("/api/clients/projects/get", {
   method: "POST",
   headers: {
     Authorization: `Bearer ${authStore.accessToken}`,
@@ -80,5 +93,17 @@ function getClientName(clientId: string) {
     (client: Client) => client._id === clientId
   );
   return client?.company;
+}
+
+async function deleteProject(projectId: string) {
+  console.log(projectId);
+  await $fetch(`/api/clients/projects/${projectId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${authStore.accessToken}`,
+      userid: authStore.userId,
+    },
+  });
+  refresh();
 }
 </script>
