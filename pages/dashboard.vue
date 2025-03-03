182e7c328d1dad51ecd3644614102352ff69a1e2
MogliCodes <moglicodes@gmail.com>
2025-03-03 11:31:34 +0100

    fix: make Dashboard mobile friendly

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,257 +1,73 @@
 <template>
-  <div class="flex w-full flex-col gap-4">
-    <!-- Onboarding teaser -->
-    <section
-      v-if="showTourTeaser"
-      class="relative overflow-hidden rounded-lg bg-yellow-300 p-12 text-yellow-950 shadow-lg"
-    >
-      <div class="w-2/3">
-        <BaseHeadline
-          class="mb-6 !text-yellow-950 dark:!text-yellow-950"
-          type="h1"
-          text="Professinal invoices made easy"
-        />
-        <p class="mb-6 text-xl">
-          Explore the simplicity of creating invoices with our quick tour. Learn
-          easy steps to effortlessly generate invoices and enhance your
-          invoicing experience. Take the tutorial now for a hassle-free
-          invoicing journey.
-        </p>
-        <BaseButton text="Start the tour" variant="amber" />
-        <div
-          class="absolute right-6 top-6 flex size-9 items-center justify-center rounded-lg bg-yellow-400"
-        >
-          <UIcon
-            class="text-2xl"
-            name="i-heroicons-x-mark"
-            @click="showTourTeaser = false"
-          />
+  <div class="flex flex-col gap-4">
+    <section v-if="showTourTeaser" id="tutorial">
+      <BaseBox class="relative overflow-hidden bg-yellow-300 !p-8 lg:!p-12 text-yellow-950">
+        <div class="md:w-2/3">
+          <BaseHeadline class="mb-6 !text-yellow-950 dark:!text-yellow-950" type="h1"
+            text="Professional invoices made easy" />
+          <p class="mb-6 text-xl">
+            Explore the simplicity of creating invoices with our quick tour.
+            Learn easy steps to effortlessly generate invoices and enhance
+            your invoicing experience. Take the tutorial now for a hassle-free
+            invoicing journey.
+          </p>
+          <BaseButton text="Start the tour" variant="amber" />
+          <div class="absolute right-6 top-6 flex size-9 items-center justify-center rounded-lg bg-yellow-400">
+            <UIcon class="text-2xl" name="i-heroicons-x-mark" @click="showTourTeaser = false" />
+          </div>
+          <UIcon class="absolute -bottom-10 right-10 hidden size-[300px] md:block"
+            name="i-heroicons-document-chart-bar" />
         </div>
-        <UIcon
-          class="absolute -bottom-10 right-10 size-[300px]"
-          name="i-heroicons-document-chart-bar"
-        />
-      </div>
+      </BaseBox>
     </section>
     <section class="flex flex-col gap-2">
       <BaseNote v-if="!validUserData">
         <p>Bitte vervollständige dein Nutzerprofil</p>
-        <nuxt-link class="font-bold text-blue-600" to="/settings"
-          >Zu den Einstellungen
-        </nuxt-link>
+        <nuxt-link class="font-bold text-blue-600" to="/settings">Zu den Einstellungen</nuxt-link>
       </BaseNote>
       <BaseNote v-if="!validCompanyData">
-        <p>
-          Die Angaben zu deinem Unternehmen sind unvollständig. Bitte
-          vervollständige deine Unternehmensdaten, um Rechnungen zu erstellen.
-        </p>
-        <nuxt-link class="font-bold text-blue-600" to="/settings#company"
-          >Zu den Einstellungen
-        </nuxt-link>
+        <p>Die Angaben zu deinem Unternehmen sind unvollständig. Bitte vervollständige deine Unternehmensdaten, um Rechnungen zu erstellen.</p>
+        <nuxt-link class="font-bold text-blue-600" to="/settings#company">Zu den Einstellungen</nuxt-link>
       </BaseNote>
       <BaseNote v-if="!validBankData">
-        <p>
-          Die Angaben zu deiner Bankverbindung sind unvollständig. Bitte füge
-          die Bankverbindung hinzu, um Rechnungen zu erstellen.
-        </p>
-        <nuxt-link class="font-bold text-blue-600" to="/settings#bank-info"
-          >Zu den Einstellungen
-        </nuxt-link>
+        <p>Die Angaben zu deiner Bankverbindung sind unvollständig. Bitte füge die Bankverbindung hinzu, um Rechnungen zu erstellen.</p>
+        <nuxt-link class="font-bold text-blue-600" to="/settings#bank-info">Zu den Einstellungen</nuxt-link>
       </BaseNote>
     </section>
-    <!-- Overview -->
-    <section class="">
-      <BaseHeadline
-        class="!mb-1 font-syne text-lg font-bold text-gray-400 lg:text-2xl"
-        type="h2"
-        text="Overview"
-      />
+    <section id="overview">
+      <BaseHeadline class="!mb-1 font-syne text-lg font-bold text-gray-400 lg:text-2xl" type="h2" text="Overview" />
       <BaseBox>
-        <div class="grid-cols-3 gap-3 md:grid">
-          <NuxtLink
-            to="/contacts"
-            class="mb-2 flex justify-between border-b-2 pb-2 pr-3 md:border-r-2 dark:border-gray-600"
-          >
-            <div>
-              <BaseLabel text="Kontakte" />
-              <span
-                class="mb-2 block font-syne text-5xl font-bold text-secondary-100"
-                >{{ contactCount }}</span
-              >
-            </div>
-            <UIcon
-              class="size-20 text-gray-300 dark:text-gray-600"
-              name="i-heroicons-user-circle"
-            />
-          </NuxtLink>
-          <NuxtLink
-            to="/invoices"
-            class="mb-2 flex justify-between border-b-2 pb-2 md:border-r-2 dark:border-gray-600"
-          >
-            <div>
-              <BaseLabel text="Rechnungen" />
-              <span
-                class="mb-2 block font-syne text-5xl font-bold text-secondary-100"
-                >{{ invoiceCount }}</span
-              >
-            </div>
-            <UIcon
-              class="size-20 text-gray-300 dark:text-gray-600"
-              name="i-heroicons-document-chart-bar"
-            />
-          </NuxtLink>
-          <NuxtLink to="/clients" class="flex justify-between">
-            <div>
-              <BaseLabel text="Kunden" />
-              <span
-                class="mb-2 block font-syne text-5xl font-bold text-secondary-100"
-                >{{ clientCount }}</span
-              >
-            </div>
-            <UIcon
-              class="size-20 text-gray-300 dark:text-gray-600"
-              name="i-heroicons-building-office"
-            />
-          </NuxtLink>
+        <div class="lg:grid lg:grid-cols-3 lg:gap-4">
+          <BaseWidget v-for="widget in overviewWidgets" :label="widget.label" :value="widget.value"
+            :icon="widget.icon" />
         </div>
       </BaseBox>
     </section>
-    <!-- Revenues -->
-    <section class="">
-      <BaseHeadline
-        class="mb-3 font-syne text-2xl font-bold text-gray-400"
-        type="h2"
-        text="Revenues"
-      />
+    <section id="revenues">
+      <BaseHeadline class="!mb-1 font-syne text-lg font-bold text-gray-400 lg:text-2xl" type="h2" text="Revenues" />
       <BaseBox>
-        <div class="grid grid-cols-3 gap-3">
-          <article
-            class="flex justify-between border-r-2 pr-3 dark:border-gray-600"
-          >
-            <div>
-              <BaseLabel text="This month" />
-              <span
-                class="mb-2 block font-syne text-5xl font-bold text-secondary-100"
-                >{{
-                  formatCurrencyAmount(formatCentToAmount(currentMonthRevnue))
-                }}</span
-              >
-            </div>
-            <UIcon
-              class="size-20 text-gray-300 dark:text-gray-600"
-              name="i-heroicons-arrow-trending-up"
-            />
-          </article>
-          <article
-            class="flex justify-between border-r-2 px-3 dark:border-gray-600"
-          >
-            <div>
-              <BaseLabel text="This quarter" />
-              <span
-                class="mb-2 block font-syne text-5xl font-bold text-secondary-100"
-                >{{
-                  formatCurrencyAmount(formatCentToAmount(currentQuarterRevnue))
-                }}</span
-              >
-            </div>
-            <UIcon
-              class="size-20 text-gray-300 dark:text-gray-600"
-              name="i-heroicons-calendar-days"
-            />
-          </article>
-          <article class="flex justify-between pl-3">
-            <div>
-              <BaseLabel text="This year" />
-              <span
-                class="mb-2 block font-syne text-5xl font-bold text-secondary-100"
-                >{{
-                  formatCurrencyAmount(formatCentToAmount(currentYearRevnue))
-                }}</span
-              >
-            </div>
-            <UIcon
-              class="size-20 text-gray-300 dark:text-gray-600"
-              name="i-heroicons-chart-bar"
-            />
-          </article>
+        <div class="lg:grid lg:grid-cols-3 lg:gap-4">
+          <BaseWidget v-for="widget in revenueWidgets" :label="widget.label" :value="widget.value"
+            :icon="widget.icon" />
         </div>
       </BaseBox>
     </section>
-    <!-- Revenue graph -->
-    <section v-if="false" class="">
+    <section id="taxes">
+      <BaseHeadline class="!mb-1 font-syne text-lg font-bold text-gray-400 lg:text-2xl" type="h2" text="Taxes" />
       <BaseBox>
-        <TestGraph :values="revenues" />
-      </BaseBox>
-    </section>
-    <!-- Taxes -->
-    <section>
-      <BaseHeadline
-        text="Taxes"
-        class="mb-3 font-syne text-2xl font-bold text-gray-400"
-        type="h2"
-      />
-      <BaseBox>
-        <div class="grid grid-cols-3 gap-3">
-          <article
-            class="flex justify-between border-r-2 pr-3 dark:border-gray-600"
-          >
-            <div>
-              <BaseLabel text="This month" />
-              <span
-                class="mb-2 block font-syne text-5xl font-bold text-secondary-100"
-                >{{
-                  formatCurrencyAmount(formatCentToAmount(currentMonthTaxes))
-                }}</span
-              >
-            </div>
-            <UIcon
-              class="size-20 text-gray-300 dark:text-gray-600"
-              name="i-heroicons-arrow-trending-up"
-            />
-          </article>
-          <article
-            class="flex justify-between border-r-2 px-3 dark:border-gray-600"
-          >
-            <div>
-              <BaseLabel text="This quarter" />
-              <span
-                class="mb-2 block font-syne text-5xl font-bold text-secondary-100"
-                >{{
-                  formatCurrencyAmount(formatCentToAmount(currentQuarterTaxes))
-                }}</span
-              >
-            </div>
-            <UIcon
-              class="size-20 text-gray-300 dark:text-gray-600"
-              name="i-heroicons-calendar-days"
-            />
-          </article>
-          <article class="flex justify-between pl-3">
-            <div>
-              <BaseLabel text="This year" />
-              <span
-                class="mb-2 block font-syne text-5xl font-bold text-secondary-100"
-                >{{
-                  formatCurrencyAmount(formatCentToAmount(currentYearTaxes))
-                }}</span
-              >
-            </div>
-            <UIcon
-              class="size-20 text-gray-300 dark:text-gray-600"
-              name="i-heroicons-chart-bar"
-            />
-          </article>
+        <div class="lg:grid lg:grid-cols-3 lg:gap-4">
+          <BaseWidget v-for="widget in taxesWidgets" :label="widget.label" :value="widget.value" :icon="widget.icon" />
         </div>
       </BaseBox>
     </section>
     <!-- User data -->
-    <section v-if="debug">
+    <section v-if="debug" id="debug">
       <BaseContainer>
         <BaseBox>
           <div class="text-xl dark:text-white">User data</div>
-          <pre class="dark:text-white"> {{ userData }}</pre>
-          <pre class="dark:text-white"> {{ authStore.accessToken }}</pre>
-          <pre class="dark:text-white"> {{ accessTokenCookie }}</pre>
+          <pre class="dark:text-white">{{ userData }}</pre>
+          <pre class="dark:text-white">{{ authStore.accessToken }}</pre>
+          <pre class="dark:text-white">{{ accessTokenCookie }}</pre>
         </BaseBox>
       </BaseContainer>
     </section>
@@ -264,15 +80,14 @@ import { useAuthStore } from "~/stores/auth.store";
 import TestGraph from "~/components/TestGraph.vue";
 import "vue-sticky-element/css";
 
-const authStore = useAuthStore(); // this is required for default styles to work.
-
 definePageMeta({
   title: "Dashboard",
+  layout: "mobilefirst",
 });
 
-useHead({
-  title: "Dashboard",
-});
+const authStore = useAuthStore(); // this is required for default styles to work.
+
+const showTourTeaser = ref(true);
 
 const { data: contactCount } = useFetch(`/api/contacts/count`, {
   method: "POST",
@@ -301,7 +116,7 @@ const { data: clientCount } = useFetch(`/api/clients/count`, {
   },
 });
 
-const { data: currentMonthRevnue } = useFetch(`/api/invoices/revenue/month`, {
+const { data: currentMonthRevenue } = useFetch(`/api/invoices/revenue/month`, {
   method: "POST",
   headers: {
     Authorization: `Bearer ${authStore.accessToken}`,
@@ -309,7 +124,7 @@ const { data: currentMonthRevnue } = useFetch(`/api/invoices/revenue/month`, {
   },
 });
 
-const { data: currentQuarterRevnue } = useFetch(
+const { data: currentQuarterRevenue } = useFetch(
   `/api/invoices/revenue/quarter`,
   {
     method: "POST",
@@ -320,7 +135,7 @@ const { data: currentQuarterRevnue } = useFetch(
   }
 );
 
-const { data: currentYearRevnue } = useFetch(`/api/invoices/revenue/year`, {
+const { data: currentYearRevenue } = useFetch(`/api/invoices/revenue/year`, {
   method: "POST",
   headers: {
     Authorization: `Bearer ${authStore.accessToken}`,
@@ -352,6 +167,73 @@ const { data: currentYearTaxes } = useFetch(`/api/invoices/tax/year`, {
   },
 });
 
+type Widget = {
+  label: string;
+  value: number;
+  icon: string;
+};
+
+const overviewWidgets = computed<Array<Widget>>(() => [
+  {
+    label: "Kontakte",
+    value: contactCount.value ?? 0,
+    icon: "i-heroicons-user-circle",
+  },
+  {
+    label: "Rechnungen",
+    value: invoiceCount.value ?? 0,
+    icon: "i-heroicons-document-text",
+  },
+  {
+    label: "Kunden",
+    value: clientCount.value ?? 0,
+    icon: "i-heroicons-building-office",
+  },
+]);
+
+const revenueWidgets = computed<Array<Widget>>(() => [
+  {
+    label: "Aktueller Monat",
+    value:
+      formatCurrencyAmount(formatCentToAmount(currentMonthRevenue.value)) ?? 0,
+    icon: "i-heroicons-arrow-trending-up",
+  },
+  {
+    label: "Aktuelles Quartal",
+    value:
+      formatCurrencyAmount(formatCentToAmount(currentQuarterRevenue.value)) ??
+      0,
+    icon: "i-heroicons-calendar-days",
+  },
+  {
+    label: "Aktuelles Jahr",
+    value:
+      formatCurrencyAmount(formatCentToAmount(currentYearRevenue.value)) ?? 0,
+    icon: "i-heroicons-chart-bar",
+  },
+]);
+
+const taxesWidgets = computed<Array<Widget>>(() => [
+  {
+    label: "Aktueller Monat",
+    value:
+      formatCurrencyAmount(formatCentToAmount(currentMonthTaxes.value)) ?? 0,
+    icon: "i-heroicons-arrow-trending-up",
+  },
+  {
+    label: "Aktuelles Quartal",
+    value:
+      formatCurrencyAmount(formatCentToAmount(currentQuarterTaxes.value)) ?? 0,
+    icon: "i-heroicons-calendar-days",
+  },
+  {
+    label: "Aktuelles Jahr",
+    value:
+      formatCurrencyAmount(formatCentToAmount(currentYearTaxes.value)) ?? 0,
+    icon: "i-heroicons-chart-bar",
+  },
+]);
+
 const { data: revenues } = useFetch("/api/revenues/get", {
   method: "POST",
   params: {
@@ -382,8 +264,6 @@ const { data: userData } = useFetch<UserData>(
     },
   }
 );
-
-const showTourTeaser = ref(true);
 
 const accessTokenCookie = useCookie("accessToken");
 
